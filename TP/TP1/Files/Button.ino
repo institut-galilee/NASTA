0115e8c8d3e4c4706975c3b44e79cba2680db3f7
nstarek <56650839+nstarek@users.noreply.github.com>
2019-11-17 17:34:38 +0100

    Add files via upload

---
+++ TP/TP1/Files/Button.ino
@@ -0,0 +1,22 @@
+/*
+NAIT SAADA TAREK 
+MARC JEAN PIERRE 
+MESSAOUDI NASSIM
+*/
+const int buttonPin = 2;    
+const int ledPin =  13;      
+int buttonState = 0;        
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
+
---
+++ TP/TP1/Files/Passive_buzzer.ino
@@ -0,0 +1,16 @@
+/*
+NAIT SAADA TAREK 
+MARC JEAN PIERRE 
+MESSAOUDI NASSIM
+*/
+
+
+#define buzzer 5
+void setup(){
+  //Null
+}
+void loop() {
+  // generates a 400Hz tone in output pin 8 with 2000ms of duration
+  tone(buzzer, 400, 2000); 
+}
+
