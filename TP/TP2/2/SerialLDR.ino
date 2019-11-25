72fc991b52ca8fd97e084c75b386bef1def91ba0
msnassim <56651736+msnassim@users.noreply.github.com>
2019-11-25 02:11:12 +0100

    Update SerialLDR.ino
    Added description comment + added missing bracket

--- TP/TP2/2/SerialLDR.ino
+++ TP/TP2/2/SerialLDR.ino
@@ -1,3 +1,4 @@
+/* Arduino code to check for the values coming from the LDR via the Arduino on pin A0 */
 
 const int sensor_pin = A0; 
 int sensor_value = 0; 
@@ -5,9 +6,9 @@ int sensor_value = 0;
 
 void setup() {
   Serial.begin(9600); 
+}
 
-void loop() {
-  
+void loop() {  
   sensor_value = analogRead(sensor_pin); 
   Serial.println(sensor_value); 
   delay(100);
