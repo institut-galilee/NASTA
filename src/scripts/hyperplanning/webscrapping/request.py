2b2e2f52e219a5235bb4761cfa6aa575f48b0689
msnassim <56651736+msnassim@users.noreply.github.com>
2019-11-28 04:07:06 +0100

    Add files via upload
    Script that forms a JSON request (POST method) and sends it to target URL (Hyperplanning) to get a  response JSON file (json-responses/edt_formation_response or others) which contains the weekly "Formation" calendar.

---
+++ src/scripts/hyperplanning/webscrapping/request.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import requests
+import json
+
+url = "https://hyperplanning.univ-paris13.fr/hp2109/invite/"
+
+session = requests.Session()
+numeroOrdre = '45C48CCE2E2D7FBDEA1AFC51C7C6AD26fb1600e1708b109e5ef12bcb46c8d631' # Doesn't work obv
+print(str(session))
+
+jar = requests.cookies.RequestsCookieJar()
+jar.set('resolution','1920x1080')
+session.cookies = jar
+
+r = session.get(url)
+jasondata = json.loads(str(r.text)
+print(r)
+
+
+"""
+Problem with numerOrdre which is encrypted ...
+- Maybe simulate a browser (browserify & nodejs maybe puppeteer..) to get missing field 
+- Or extract json from webpage and use it as PoC ... 
+"""
+
+
