fe7db77381871a383724763dc4f4c90ed5e814d2
msnassim <56651736+msnassim@users.noreply.github.com>
2019-11-28 01:37:51 +0100

    Update occupation.py
    - Cleaned the server's reponse to convert it to json format
    - Added try - except when writing to the new .json file

--- src/scripts/occupation-salles/occupation.py
+++ src/scripts/occupation-salles/occupation.py
@@ -30,7 +30,7 @@ def main():
          outfile = arg
       elif opt in ("-v", "--verbose"):
          verbose = 1
-         
+
    final_url = base_url + 'grain=' + grain + '&debut=' + debut + '&fin=' + fin + '&tqx=' + reqID
    
    print('\n')
@@ -45,12 +45,20 @@ def main():
       print('[-] Reason: ', e.reason)
    else:
       print('[+] Server is UP and responded to your request')      
-      with open(outfile, 'w') as out_file:
-         data = response.read()
-         out_file.write(str(data))
-         print('[+] Successfully written data to '+ outfile)
-         if verbose == 1 : 
-            print(data)
+      try:   
+         with open(outfile, 'w') as out_file:
+            data_to_string = str(response.read())
+            data_to_delete = "bgoogle.visualization.Query.setResponse("
+            len_ = len(data_to_delete)
+            json_output = data_to_string[len_ + 1:-3]
+            print('[+] Cleaned request to convert it to json format')
+            out_file.write(json_output )
+            print('[+] Successfully written data to '+ outfile + '\n')
+            if verbose == 1 : 
+               print('[+] Generated your request in json format\n')
+               print(json_output+'\n')
+      except IOError as e:
+         print("Error: ", e, "Check permissions !")     
 
 
 if __name__ == "__main__":
