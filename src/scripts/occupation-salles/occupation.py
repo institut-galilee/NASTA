2fdd464ce727ee4ea5c4f1e4aa0fccc05d17fe31
msnassim <56651736+msnassim@users.noreply.github.com>
2019-12-05 21:43:34 +0100

    Update occupation.py
    Cleaned the code a bit : 
     - New auxiliary functions.
     - Added some comments
     - Global structure of the code is now fixed.

--- src/scripts/occupation-salles/occupation.py
+++ src/scripts/occupation-salles/occupation.py
@@ -1,11 +1,57 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys, getopt
 import urllib.request
 from urllib.error import URLError, HTTPError
 
-def main():
+# Usage example : python3 occupation.py -g quart -d "2019-12-04" -f "2019-12-04" -r reqId:1 -v -o data.json 
 
+"""
+   Cleans the response and the write to the json file provided as an argument (also has verbose mode with the "-v" option)
+"""
+def jsonresponse2file(file, response)
+   
+   try:   
+      with open(file, 'w') as out_file:
+            data_to_string = str(response.read())
+            json_output = clean_request_data(data_to_string)
+            print('[+] Cleaned request to convert it to json format')
+            out_file.write(json_output )
+            print('[+] Successfully written data to '+ file + '\n')
+            if verbose == 1 : 
+               print('[+] Generated your request in json format\n')
+               print(json_output+'\n')
+   except IOError as e:
+         print("Error: ", e, "Check permissions !")   
+
+"""
+   Dumps info to the user about the GET request he's sending
+"""
+def dumpConsoleInfo(base_url, final_url):
+   print('\n')
+   print('[+] GET request sent to ' + base_url + ' with the passed arguments ...')
+   print('[+] Target Url is : ', final_url)
+   
+"""
+   Constructs the URL the user requested
+"""
+def construct_final_url(base_url,grain,debut,fin,reqID):
+   final_url =  base_url + 'grain=' + grain + '&debut=' + debut + '&fin=' + fin + '&tqx=' + reqID
+   return final_url
+
+"""
+   Strip unwanted content from data grabbed from the server's response, to make it JSON-readable
+"""
+def clean_request_data(data):
+   data_to_delete = "bgoogle.visualization.Query.setResponse("
+   len_ = len(data_to_delete)
+   json_output = data_to_string[len_ + 1:-3]
+   return json_output
+ 
+   def
+
+if __name__ == "__main__":
+   
    verbose = 0
    base_url = "https://si-galilee.univ-paris13.fr/salles/occupation_data?"
   
@@ -31,35 +77,16 @@ def main():
       elif opt in ("-v", "--verbose"):
          verbose = 1
 
-   final_url = base_url + 'grain=' + grain + '&debut=' + debut + '&fin=' + fin + '&tqx=' + reqID
-   
-   print('\n')
-   print('[+] GET request sent to ' + base_url + ' with the passed arguments ...')
-   print('[+] Target Url is : ', final_url)
-   
+   final_url = construct_final_url(base_url,grain,debut,fin,reqID)
+   dumpConsoleInfo(base_url, final_url)
+
    try:
       response = urllib.request.urlopen(final_url)
    except HTTPError as e:
-      print('[-] Error code: ', e.code,' ')
+      print('[-] Error code : ', e.code,' ')
    except URLError as e:
-      print('[-] Reason: ', e.reason)
+      print('[-] Reason : ', e.reason)
    else:
       print('[+] Server is UP and responded to your request')      
-      try:   
-         with open(outfile, 'w') as out_file:
-            data_to_string = str(response.read())
-            data_to_delete = "bgoogle.visualization.Query.setResponse("
-            len_ = len(data_to_delete)
-            json_output = data_to_string[len_ + 1:-3]
-            print('[+] Cleaned request to convert it to json format')
-            out_file.write(json_output )
-            print('[+] Successfully written data to '+ outfile + '\n')
-            if verbose == 1 : 
-               print('[+] Generated your request in json format\n')
-               print(json_output+'\n')
-      except IOError as e:
-         print("Error: ", e, "Check permissions !")     
-
-
-if __name__ == "__main__":
-   main()
+      jsonresponse2file(outfile, response)
+  
