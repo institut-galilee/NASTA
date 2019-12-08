914823495c104b4f1d3a8e32c11f0ff9bd8840e6
msnassim <nassim.messaoudi@edu.univ-paris13.fr>
2019-12-08 15:27:24 +0100

    restructered code -- Typos fixing

--- src/scripts/occupation-salles/getnbpeople.py
+++ src/scripts/occupation-salles/getnbpeople.py
@@ -42,59 +42,58 @@ def display_dictionnary_info(dic):
 
 def recupesalleinfo(dictionnary):
     try:
-        #connection in the database.
-        #Create if it not exist
+        #connection to the database.
+        #Creates if it doesn't exist
         connexion = sqlite3.connect("occupation_DataBase.db")
         curseur = connexion.cursor()
         #Request for CREATE TABLE 'salleinfo'
         curseur.execute('''CREATE TABLE IF NOT EXISTS salleinfo(
-        id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-        dates TEXT default (NULL),
-        total TEXT default (NULL),
-        F200 TEXT default (NULL),
-        F201 TEXT default (NULL),
-        F202 TEXT default (NULL),
-        F203 TEXT default (NULL),
-        F204 TEXT default (NULL),
-        F205 TEXT default (NULL),
-        F206 TEXT default (NULL),
-        F207 TEXT default (NULL),
-        G207 TEXT default (NULL),
-        G208 TEXT default (NULL),
-        G209 TEXT default (NULL),
-        G210 TEXT default (NULL),
-        G211 TEXT default (NULL),
-        G212 TEXT default (NULL),
-        G215 TEXT default (NULL)
-        )''')
-        
-        #Type: list
-        #Description: Names of our columns in the table, we will use it on the request.
+                           id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                           dates TEXT default (NULL),
+                           total TEXT default (NULL),
+                           F200 TEXT default (NULL),
+                           F201 TEXT default (NULL),
+                           F202 TEXT default (NULL),
+                           F203 TEXT default (NULL),
+                           F204 TEXT default (NULL),
+                           F205 TEXT default (NULL),
+                           F206 TEXT default (NULL),
+                           F207 TEXT default (NULL),
+                           G207 TEXT default (NULL),
+                           G208 TEXT default (NULL),
+                           G209 TEXT default (NULL),
+                           G210 TEXT default (NULL),
+                           G211 TEXT default (NULL),
+                           G212 TEXT default (NULL),
+                           G215 TEXT default (NULL))'''
+                        )
+        # Type: list
+        # Description: Names of our columns in the table, we will use this list for the request.
         liste_id=['dates','total','F200','F201','F202','F203','F204','F205','F206','F207','G207','G208','G209','G210','G211','G212','G215']
 
-        #Type: list
-        #Descition: use for extract value from dictionnary.
+        # Type: list
+        # Description: use for extract value from dictionnary.
         values=[]
         i=0
         for row in dictionnary['table']['rows']:          
             for row2 in row.values():
               j=0
-              #Type: tuple
-              #Description: that tuple will containt all the values will need on the request.
+              # Type: tuple
+              # Description: that tuple will contain all the values we'll need for the request.
               placeholders=()    
               columns = ', '.join(liste_id) #create columns list for the request.
               values.append(row2)
-              liste_value=values[i] #the value is a dict, then it's a list of [dictionnary of dictionnary].
+              liste_value=values[i] # the value is a dict, then it's a list of [dictionnary of dictionnary].
               for cmpt in liste_value:
-                listedict=liste_value[j] #extract the last dictionnary, then is juste a list of dictionnary.
-                valeurdict=(str(listedict['v'])) #here extract the value of the dictionnary.            
-                placeholders = placeholders +(valeurdict,) #insert the value in the tuple for the request.
+                listedict=liste_value[j] # extract the last dictionnary, then is just a list of dictionnary.
+                valeurdict=(str(listedict['v'])) # here extract the value from the dictionnary.            
+                placeholders = placeholders +(valeurdict,) # insert the value in the tuple for the request.
                 j=j+1
               query = 'INSERT INTO salleinfo (%s) VALUES %s;' % (columns,  placeholders)                
               curseur.execute(query)
               connexion.commit()
               i=i+1    
-        print("Database crée.")
+        print("[+] Successfully created and filled database ! ")
        
     except sqlite3.Error as e:
             self.log.error("Database error: %s" % e)
@@ -111,9 +110,6 @@ def json2dict(file):
   except IOError as e: 
     print("Error: ",e)
 
-#keys = {"cols","rows"}
-#dict_new = {x:d[x] for x in d['table']}
-#print(dict_new)
 
 if __name__ == "__main__":
   
@@ -131,7 +127,7 @@ if __name__ == "__main__":
     elif opt in ("-i", "--input"):
       file = arg
   dictionnary = json2dict(file)
- # display_dictionnary_info(dictionnary)
+  #display_dictionnary_info(dictionnary)
   recupesalleinfo(dictionnary)
   
 
