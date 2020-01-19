158d61288df591f9a29da620b82d87029366f61c
msnassim <56651736+msnassim@users.noreply.github.com>
2020-01-19 23:35:31 +0100

    Add files via upload
    Scripts to start and stop ldap-server-container (used Docker here)

---
+++ src/LDAP/Server/docker-openldap.sh
@@ -0,0 +1,2 @@
+sudo docker run -it --rm --name openldapmyp13            -p 389:389            -e DEBUG_LEVEL=1            -e DOMAIN=localhost            -e ORGANIZATION=MyP13            -e PASSWORD=1234            mwaeckerlin/openldap
+
---
+++ src/LDAP/Server/stop-ldapserver-container.sh
@@ -0,0 +1,1 @@
+sudo docker stop openldapmyp13
